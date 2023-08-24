f2dcefa6279d4aa82d7d0c2903dcbd630f0b4c20
AdamW <curioussavage@protonmail.com>
2023-08-24 14:13:39 -0600

    Add tests for confirm-dialog

--- client/src/app/confirm-dialog/confirm-dialog.component.spec.ts
+++ client/src/app/confirm-dialog/confirm-dialog.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppModule } from '../app.module';
 
-import {MatDialogHarness} from '@angular/material/dialog/testing';
 import { MatDialogModule, MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { ConfirmDialogComponent } from './confirm-dialog.component';
@@ -13,13 +12,15 @@ describe('ConfirmDialogComponent', () => {
   let component: ConfirmDialogComponent;
   let fixture: ComponentFixture<ConfirmDialogComponent>;
 
+  let closeSpy: jasmine.Spy
   beforeEach(async () => {
+    closeSpy = jasmine.createSpy('close');
     await TestBed.configureTestingModule({
       imports: [AppModule, MatDialogModule],
       declarations: [ ConfirmDialogComponent ],
       providers: [
         {provide: MAT_DIALOG_DATA, useValue: { message: "test" }},
-        {provide: MatDialogRef, useValue: { close: (res: any) => res }}
+        {provide: MatDialogRef, useValue: { close: closeSpy }}
       ]
     })
     .compileComponents();
@@ -30,7 +31,24 @@ describe('ConfirmDialogComponent', () => {
   });
 
   it('should create', async () => {
-    MatDialogHarness
     expect(component).toBeTruthy();
+  });
+
+  it('should display the message', async () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.textContent)
+      .toContain("test");
+  });
+
+  it('should close dialog on confirm clicked', () => {
+    component.onConfirm();
+    fixture.detectChanges();
+    expect(closeSpy.calls.count()).toBe(1, 'dialog closed');
+  });
+
+  it('should close dialog on Cancel clicked', () => {
+    component.onCancel();
+    fixture.detectChanges();
+    expect(closeSpy.calls.count()).toBe(1, 'dialog closed');
   });
 });
